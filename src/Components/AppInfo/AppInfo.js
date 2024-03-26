a75d469f17e4450fe0a260ffdbdc2967afb7bd22
Mahalakshmi-Devaraj <magalakshmi.d@asmltd.com>
2024-03-26 17:04:25 +0530

    Diagram

---
+++ src/Components/AppInfo/AppInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './AppInfo.css';
+
+function AppInfo(){
+    return(
+        <div class="body" >
+            <div class="card-body">
+                <h3 class="card-title">APP INFO</h3>
+                <p class="card-text"><span>Time :</span>  12:12 AM</p>
+                <p class="card-text"><span>Supported platform:</span> DELL</p>
+                <p class="card-text"><span>testcase count :</span> 4</p>
+            </div>
+        </div>
+    )
+}
+export default AppInfo;
