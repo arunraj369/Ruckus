213b57a2f6b3c20c220ba42e64a24e21e67d558c
Arunraj <102640467+arunraj369@users.noreply.github.com>
2024-03-26 13:09:54 +0530

    Login and signup pages done

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import "./Login.css";
+import loginimg from "../assets/Login1.png";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function Login() {
+  const [loginUser, setloginUser] = useState({
+    loginEmail: "",
+    loginPassword: "",
+  });
+
+  function handleChange(e) {
+    const newloginUser = { ...loginUser };
+    newloginUser[e.target.id] = e.target.value;
+    setloginUser(newloginUser);
+    // console.log(newloginUser);
+  }
+
+  const logInData = async (e) => {
+    e.preventDefault();
+
+    if (!loginUser.loginEmail === "" || !loginUser.loginPassword) {
+      toast.error("Please fill in all fields.");
+      return;
+    }
+
+    try {
+      const response = await axios.get("http://localhost:3001/User");
+      const users = response.data;
+
+      const validateUser = users.find(
+        (user) =>
+          user.email === loginUser.loginEmail &&
+          user.password === loginUser.loginPassword
+      );
+
+      if (validateUser) {
+        toast.success("Login successfully");
+      } else {
+        toast.error("Incorrect Email or Password");
+        setloginUser({
+          loginEmail: "",
+          loginPassword: "",
+        });
+      }
+      // console.log(users);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  return (
+    <div class="container-fluid">
+      <div
+        class="card row flex-md-row flex-column"
+        style={{ backgroundColor: "  #e5e5e5", borderRadius: "25px" }}
+      >
+        <div class="col-md img_div">
+          <img src={loginimg} alt="no-img" className="logimg" />
+        </div>
+
+        <div class="col-md loginform_div mt-3">
+          <div style={{ textAlign: "center", marginTop: "7%" }}>
+            <h3>Login</h3>
+          </div>
+          <div className="mt-5">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="loginEmail"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                  value={loginUser.loginEmail}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="loginPassword"
+                  placeholder="Password"
+                  value={loginUser.loginPassword}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={logInData}
+                style={{ marginLeft: "43%" }}
+              >
+                Login
+              </button>
+            </form>
+            <ToastContainer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import "./Signup.css";
+import loginimg from "../assets/Signup.png";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function Signup() {
+  const [User, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const signUpData = async (e) => {
+    e.preventDefault();
+
+    if (!User.email || !User.password) {
+      toast.error("Please fill in all fields.");
+      return;
+    }
+
+    try {
+      await axios.post("http://localhost:3001/User", User);
+      toast.success("User registered successfully!");
+      //   alert("User registered successfully!");
+      setUser({ email: "", password: "" }); // Clear form fields after submission
+    } catch (error) {
+      console.error("Error registering user:", error);
+      toast.error("Error registering user. Please try again.");
+    }
+  };
+
+  function handleChange(e) {
+    const newuser = { ...User };
+    newuser[e.target.id] = e.target.value;
+    setUser(newuser);
+    console.log(newuser);
+  }
+
+  return (
+    <div class="container-fluid">
+      <div
+        class="card row flex-md-row flex-column"
+        style={{ backgroundColor: "  #e5e5e5", borderRadius: "25px" }}
+      >
+        <div class="col-md img_div">
+          <img src={loginimg} alt="no-img" className="logimg" />
+        </div>
+
+        <div class="col-md loginform_div mt-3">
+          <div style={{ textAlign: "center", marginTop: "7%" }}>
+            <h3>Signup</h3>
+          </div>
+          <div className="mt-5">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                  value={User.email}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={User.password}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={signUpData}
+                style={{ marginLeft: "43%" }}
+              >
+                Signup
+              </button>
+            </form>
+            <ToastContainer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
